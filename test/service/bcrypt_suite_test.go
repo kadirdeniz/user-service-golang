8ed9db217fa40688ad4b3f7fef5c8f4680f4f403
Kadir Deniz <kadirdenz@hotmail.com>
2023-08-18 14:44:57 +0300

    service tests

--- test/service/bcrypt_suite_test.go
+++ test/service/bcrypt_suite_test.go
@@ -10,25 +10,23 @@ import (
 
 var testingObj *testing.T
 
-func TestAuthHandler(t *testing.T) {
+func TestBcryptService(t *testing.T) {
 	testingObj = t
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Bcrypt Suite")
 }
 
-var _ = Describe("Auth Handler", func() {
-	Describe("Hash Password", Ordered, func() {
-		bcryptService := service.NewBcrypt()
+var _ = Describe("Bcrypt Service", func() {
+	bcryptService := service.NewBcrypt()
 
-		It("Should return a hashed password", func() {
-			hashedPassword := bcryptService.HashPassword("password")
-			Expect(hashedPassword).ToNot(BeNil())
-		})
+	It("Should return a hashed password", func() {
+		hashedPassword := bcryptService.HashPassword("password")
+		Expect(hashedPassword).ToNot(BeNil())
+	})
 
-		It("Should check is passwords match", func() {
-			hashedPassword := bcryptService.HashPassword("password")
-			isMatch := bcryptService.IsPasswordCorrect(hashedPassword, "password")
-			Expect(isMatch).To(BeTrue())
-		})
+	It("Should check is passwords match", func() {
+		hashedPassword := bcryptService.HashPassword("password")
+		isMatch := bcryptService.IsPasswordCorrect(hashedPassword, "password")
+		Expect(isMatch).To(BeTrue())
 	})
 })
---
+++ test/service/jwt_suite_test.go
@@ -0,0 +1,45 @@
+package service_test
+
+import (
+	"testing"
+	"user-service-golang/internal/service"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestJWTHandler(t *testing.T) {
+	testingObj = t
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "JWT Suite")
+}
+
+var _ = Describe("JWT Service", func() {
+	var jwt service.JWTActions
+
+	BeforeEach(func() {
+		jwt = service.NewJWT()
+	})
+
+	Context("Set User ID", func() {
+		It("should set user id", func() {
+			jwt.SetUserId(1)
+			Expect(jwt.GetUserId()).To(Equal(1))
+		})
+	})
+
+	Context("Set Token", func() {
+		It("should set token", func() {
+			jwt.SetToken("token")
+			Expect(jwt.GetToken()).To(Equal("token"))
+		})
+	})
+
+	Context("Generate Token", func() {
+		It("should parse token", func() {
+			mockToken := jwt.SetUserId(9).CreateToken()
+			claims := mockToken.ParseToken()
+			Expect(claims.GetUserId()).To(Equal(uint(9)))
+		})
+	})
+})
